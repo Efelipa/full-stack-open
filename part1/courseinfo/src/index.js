4e22381637685fc313f5595a1bcdc1e976036642
Erick Felipa <erickfelipa@gmail.com>
2023-04-16 08:57:17 -0500

    Fixing the code issue

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,46 +1,52 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
+// Const-definitions
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
 
-// Components
-const Title = (props) => {
-  const {course} = props;
-  return <h1>{course}</h1>
-}
-const Content = (props) => {
+const Header = (prop) => {
   return (
     <div>
-      <part>
-        {props.part1}
-        <p>{props.exercises1}</p>
-      </part>
-      <part>
-        {props.part2}
-        <p>{props.exercises2}</p>
-      </part>
-      <part>
-        {props.part3}
-        <p>{props.exercises3}</p>
-      </part>
+      <h1>{prop.title}</h1>
     </div>
   )
 }
-const Footer = (props) => {
-  const result = props.exercises1 +  props.exercises2 + props.exercises3;
-  return <div>Number of exercises {result}</div>
+const Part = (parts) => {
+  return (
+    <div>
+      <p>{parts.title} {parts.exercise}</p>
+    </div>
+  )
+}
+const Content = () => {
+  return (
+    <div>
+      <Part title={part1} exercise={exercises1}/>
+      <Part title={part2} exercise={exercises2}/>
+      <Part title={part3} exercise={exercises3}/>
+    </div>
+  )
+}
+const Total = (prop) => {
+  return (
+    <div>
+      <p>Total: {prop.total}</p>
+    </div>
+  )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
 
   return (
     <>
-      <Title course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header title={course}/>
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3}/>
     </>
   )
 }
